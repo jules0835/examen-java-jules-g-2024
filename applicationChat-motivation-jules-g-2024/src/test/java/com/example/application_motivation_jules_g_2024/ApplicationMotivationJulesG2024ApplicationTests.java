15f937b1a297799a1c557066c9d9e061c4e8b52e
Jules G. <84841212+jules0835@users.noreply.github.com>
2025-02-13 16:47:47 +0100

    Update index

---
+++ applicationChat-motivation-jules-g-2024/src/test/java/com/example/application_motivation_jules_g_2024/ApplicationMotivationJulesG2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.application_motivation_jules_g_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMotivationJulesG2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
