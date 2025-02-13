b56d9d2fb369a6a5343f18f814ce8cc31873d585
Jules G. <84841212+jules0835@users.noreply.github.com>
2025-02-13 16:50:46 +0100

    Add SQL script for quotes création

---
+++ script.sql
@@ -0,0 +1,30 @@
+USE defaultdb;
+INSERT INTO quotes (message)
+VALUES (
+    'Le succès est la somme de petits efforts, répétés jour après jour.'
+  ),
+  (
+    'Ne crains pas d’échouer, crains plutôt de ne pas essayer.'
+  ),
+  (
+    'L’échec est simplement l’opportunité de recommencer, cette fois plus intelligemment.'
+  ),
+  (
+    'Le seul endroit où le succès précède le travail est dans le dictionnaire.'
+  ),
+  (
+    'Ce n’est pas parce que les choses sont difficiles que nous n’osons pas, c’est parce que nous n’osons pas qu’elles sont difficiles.'
+  ),
+  ('Croyez en vous et tout devient possible.'),
+  (
+    'Faites ce que vous pouvez, avec ce que vous avez, là où vous êtes.'
+  ),
+  (
+    'L’optimisme est la foi qui mène à la réussite. Rien ne peut être fait sans espoir et confiance.'
+  ),
+  (
+    'Lorsque vous voulez abandonner, rappelez-vous pourquoi vous avez commencé.'
+  ),
+  (
+    'Le plus grand échec est de ne pas avoir le courage d’oser.'
+  );
